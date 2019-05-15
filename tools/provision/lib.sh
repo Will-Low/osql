bfe27aa680313519105727619f88ab0f625eb60b
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2019-05-15 19:37:56 +0200

    Remove debug log
    [skip ci]

--- tools/provision/lib.sh
+++ tools/provision/lib.sh
@@ -15,8 +15,6 @@ function setup_brew() {
   BREW_TYPE="$2"
   ACTION="$3"
 
-  log "setup_brew: $DEPS $BREW_TYPE $ACTION $HOMEBREW_NO_AUTO_UPDATE"
-
   if [[ "$BREW_TYPE" == "linux" ]]; then
     BREW_REPO=$LINUXBREW_REPO
     BREW_COMMIT=$LINUXBREW_BREW
